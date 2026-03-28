089c7467290d521f6a013e67d4d796efa48fbe6d
Tim Sackton <timsackton@gmail.com>
2026-03-28 12:57:39 -0400

    Add step-0 source preparation workflow

--- cli/session.py
+++ cli/session.py
@@ -17,6 +17,7 @@ COMMAND_TO_MODULE = {
     "synchronize": "taelgar_utils.session.synchronize",
     "clean-speakers": "taelgar_utils.session.clean_speakers",
     "process-zoom": "taelgar_utils.session.workflows.zoom",
+    "prepare-source": "taelgar_utils.session.prepare_source",
 }
 
 
